9106a13b3a8dad36a5f87618f1e5c850d96797cb
poovarasan4321 <poovarasan2927@gmail.com>
2026-04-01 08:53:21 +0530

    "add admin panel"

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from tracker.models import Transaction
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    # ✅ Columns you want to show
+    list_display = ('user', 'amount', 'type',
+                    'category', 'description', 'date')
+
+
+admin.site.register(Transaction, TransactionAdmin)
 
 # Register your models here.
