0fb72db0f4c421bad2a5cbe6ca194b02b629d8e1
poovarasan4321 <poovarasan2927@gmail.com>
2026-04-01 09:16:28 +0530

    connected postgresql

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -74,13 +74,20 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        'postgresql://expense_db_t4mt_user:bhEOPgS4YmqY9OqyXpDlFhwBI8yJW5qE@dpg-d7696k5m5p6s73bhvjag-a/expense_db_t4mt'
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
