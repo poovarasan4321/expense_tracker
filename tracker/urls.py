d566893ca26cc2d9c90301d4ea98affb5d21c07e
poovarasan4321 <poovarasan2927@gamil.com>
2026-04-05 18:44:42 +0530

    fix bugs

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('add/', views.add_expense, name='add_expense'),
+    path('report/', views.report, name='report'),
 
     # FIXED
     path('edit/<int:id>/', views.edit_expense, name='edit_expense'),
